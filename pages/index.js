958b5d8141cd7eef7f806cc8728f228368af4788
Inwoo Parka <piw601625@gmail.com>
2023-01-12 23:56:06 +0900

    movie detail router

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,44 @@
-import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title: `${title}`,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <Link
+            href={{
+              pathname: `/movies/${movie.id}`,
+              query: {
+                title: `${movie.original_title}`,
+              },
+            }}
+            as={`/movies/${movie.id}`}
+            legacyBehavior
+          >
+            <a>
+              <h4>{movie.original_title}</h4>
+            </a>
+          </Link>
         </div>
       ))}
       <style jsx>
@@ -18,6 +48,9 @@ export default function Home({ results }) {
             grid-template-columns: 1fr 1fr;
             padding: 20px;
             gap: 20px;
+          }
+          .movie {
+            cursor: pointer;
           }
           .movie img {
             max-width: 100%;
